b54eff443cb4d3eafaf104412ba7341302d8e570
parkhyunseo <dazzle1999@naver.com>
2024-07-23 17:13:57 +0900

    Feat: Ticket Entity 생성

---
+++ src/main/java/com/T82/ticket/global/domain/entity/Ticket.java
@@ -0,0 +1,43 @@
+package com.T82.ticket.global.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "TICKETS")
+public class Ticket {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TICKET_ID")
+    private Long ticketId;
+    @Column(name = "USER_ID")
+    private Long userId;
+    @Column(name = "SECTION_NAME")
+    private String sectionName;
+    @Column(name = "ROW_NUM")
+    private Integer rowNum;
+    @Column(name = "COLUMN_NUM")
+    private Integer columnNum;
+    @Column(name = "IS_REFUND")
+    private boolean isRefund;
+    @Column(name = "EVENT_NAME")
+    private String eventName;
+    @Column(name = "EVENT_START_TIME")
+    private Date eventStartTime;
+    @Column(name = "PAYMENT_DATE")
+    private Date paymentDate;
+    @Column(name = "PAYMENT_AMOUNT")
+    private Long paymentAmount;
+    @Column(name = "ORDER_NUM")
+    private String orderNum;
+}
